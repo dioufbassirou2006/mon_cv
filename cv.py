d10c8b0e8a4eea50dc02fde3c45ea90c14608611
dioufbassirou2006 <dioufbassirou2006@gmail.com>
2026-02-27 12:11:09 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+with st.sidebar:
+    st.image("FALLOU.jpeg",width=300)
+    st.write("FALLOU DIOUF")
+    st.write("Email: dioufbassirou2006@gmail.com")
+    st.write("Adresse: Pikine")
+
+
+st.subheader("profil")
+st.write("Etudiant en deuxième année en géomatique, je suis doté d'une capacité de collecter des données géographiques de les traiter et de réaliser des cartes")
+
+st.subheader("Projets académiques")
+st.write("• Collecte des lampadaires au tour du CEDT LE G15 par Mobile Topographer ")
+st.write("• Collecte des points de commerce de la commune de Gueule Tapée-Fass-Colobane par Locus Map ")
+st.write("• Collecte des ecoles de la commune de Grand Dakar par QField")
+st.write("• Collecte des centres de formation professionnelles de la région de Dakar par QField")
+st.write("• Levée topographique d'une tronçon de la route de l'université par GPS" )
+st.write("• Levée topographique des départements du CEDT LE G15 par Station totale")
+st.write("• Présentation d'un projet d'entreprise")
+
+st.subheader("Compétences")
+st.write("• Conception de base de données sur QUANTUM GIS, ARCGIS, Looping, et Post gres")
+st.write("• Géoréférencement")
+st.write("• Digitalisation")
+st.write("• Capacité de collecte de données géographiques")
+st.write("• Capacité de faire des levées topographiques")
+st.write("• Traitement de données géographiques")
+st.write("• Conception  de Plans sur Autocad et Sketchup")
+st.write("• Capacité en création d'application web")
+st.write("• Capacité en bureautique")
+st.write("• Réalisation de carte")
+
+st.subheader("FORMATIONS")
+st.write("• 2024-2026:Technicien Supérieur en Geomatique au CEDT LE G15")
+
+st.subheader("Diplômes")
+st.write("• 2026:BTS en Geomatique en cours")
+st.write("• 2023:Baccalauréat en série S2")
+
+st.subheader("Expériences")
+st.write("• Vice président d'une association communautaire")
+st.write("• Gérant d'un grand boutique de commerce")
+
+
+
+
+
